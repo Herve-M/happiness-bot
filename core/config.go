a8370f44d389c6e3aa49b199aeea8b90f5fd9a0d
MATYSIAK Hervé <fossdev@hervematysiak.eu>
2016-08-16 11:41:13 +0200

    Add basic config. management

--- core/config.go
+++ core/config.go
@@ -1,5 +1,14 @@
 package core
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+	"github.com/asaskevich/govalidator"
+)
+
+const cfgName = "hb.yaml"
+
 // Flags holds the CLI configuration values and flags
 type Flags struct {
 	Verbose      bool
@@ -7,7 +16,39 @@ type Flags struct {
 	SlackWebHook string
 }
 
-// initConfig Used to get the default configuration file (default.json) and the possible
-// override conf. file. Flags override final config. only if different of genCfg.
-func initConfig(flags *Flags) {
+func init() {
+	viper.SetConfigFile(cfgName)
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+}
+
+func validateConfig() error {
+	if !govalidator.IsURL(GetSlackWebHookUrl()) {
+		return fmt.Errorf("Invalid WebHookUrl!")
+	}
+	return nil
+}
+
+func ReadConfig() error {
+	err := viper.ReadInConfig()
+	if err != nil {
+	    return fmt.Errorf("Fatal error config file: %s \n", err)
+	}
+	return validateConfig()
+}
+
+func GetSlackWebHookUrl() string {
+	return viper.GetString("slack.url")
+}
+
+func GetSlackBotName() string {
+	return viper.GetString("slack.botname")
+}
+
+func GetSlackBotIcon() string {
+	return viper.GetString("slack.boticon")
+}
+
+func GetSlackTarget() string {
+	return viper.GetString("slack.target")
 }
